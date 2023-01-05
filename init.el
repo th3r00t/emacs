9dba1c06b02b3059a3d083a0f67efa6872db8034
th3r00t <admin@mylt.dev>
2023-01-04 23:45:17 -0500

    Most recent working changes

--- init.el
+++ init.el
@@ -51,6 +51,8 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(battery-upower-device '("battery_BAT0"))
+ '(custom-safe-themes
+   '("60ada0ff6b91687f1a04cc17ad04119e59a7542644c7c59fc135909499400ab8" default))
  '(delete-selection-mode nil)
  '(display-battery-mode t)
  '(display-line-numbers-type t)
@@ -71,10 +73,11 @@
  '(lsp-ui-doc-use-childframe t)
  '(lsp-ui-doc-use-webkit nil)
  '(org-agenda-files
-   '("/home/th3r00t/org/projects.org" "/home/th3r00t/org/projects.org" "/home/th3r00t/org/journal/20230103"))
+   '("~/org/.org-roam/20230104003659-system.org" "/home/th3r00t/org/projects.org" "/home/th3r00t/org/projects.org" "/home/th3r00t/org/journal/20230103"))
  '(package-selected-packages '(## evil use-package))
  '(rustic-babel-default-toolchain "nightly")
  '(send-mail-function 'smtpmail-send-it)
+ '(undo-tree-history-directory-alist '(("." . "~/.emacs.d/undo-tree")))
  '(warning-suppress-log-types '((lsp-mode) (lsp-mode)))
  '(warning-suppress-types '((emacs) (use-package) (comp) (emacs) (lsp-mode)))
  '(which-key-frame-max-height 40))
@@ -83,4 +86,30 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(default ((t (:inherit nil :extend nil :stipple nil :background "#1a1b26" :foreground "#a9b1d6" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight regular :height 180 :width normal :foundry "CTDB" :family "FiraCode Nerd Font Mono")))))
+ '(default ((t (:inherit nil :extend nil :stipple nil :background "#1a1b26" :foreground "#a9b1d6" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight regular :height 180 :width normal :foundry "CTDB" :family "FiraCode Nerd Font Mono"))))
+ '(fixed-pitch ((t (:family "Fira Code Retina" :height 145))))
+ '(org-block ((t (:inherit fixed-pitch))))
+ '(org-code ((t (:inherit (shadow fixed-pitch)))))
+ '(org-document-info ((t (:foreground "dark orange"))))
+ '(org-document-info-keyword ((t (:inherit (shadow fixed-pitch)))))
+ '(org-document-title ((t (:inherit default :weight bold :foreground "#F9F9F9" :font "Source Sans Pro" :height 1.5 :underline nil))))
+ '(org-indent ((t (:inherit (org-hide fixed-pitch)))))
+ '(org-level-1 ((t (:inherit default :weight bold :foreground "#F9F9F9" :font "Source Sans Pro" :height 1.35))))
+ '(org-level-2 ((t (:inherit default :weight bold :foreground "#F9F9F9" :font "Source Sans Pro" :height 1.2))))
+ '(org-level-3 ((t (:inherit default :weight bold :foreground "#F9F9F9" :font "Source Sans Pro" :height 1.15))))
+ '(org-level-4 ((t (:inherit default :weight bold :foreground "#F9F9F9" :font "Source Sans Pro" :height 1.08))))
+ '(org-level-5 ((t (:inherit default :weight bold :foreground "#F9F9F9" :font "Source Sans Pro"))))
+ '(org-level-6 ((t (:inherit default :weight bold :foreground "#F9F9F9" :font "Source Sans Pro"))))
+ '(org-level-7 ((t (:inherit default :weight bold :foreground "#F9F9F9" :font "Source Sans Pro"))))
+ '(org-level-8 ((t (:inherit default :weight bold :foreground "#F9F9F9" :font "Source Sans Pro"))))
+ '(org-link ((t (:foreground "royal blue" :underline t))))
+ '(org-meta-line ((t (:inherit (font-lock-comment-face fixed-pitch)))))
+ '(org-property-value ((t (:inherit fixed-pitch))) t)
+ '(org-special-keyword ((t (:inherit (font-lock-comment-face fixed-pitch)))))
+ '(org-table ((t (:inherit fixed-pitch :foreground "#83a598"))))
+ '(org-tag ((t (:inherit (shadow fixed-pitch) :weight bold :height 0.8))))
+ '(org-verbatim ((t (:inherit (shadow fixed-pitch)))))
+ '(variable-pitch ((t (:family "ETBembo" :height 155 :weight thin)))))
+(provide 'init)
+;;; init.el ends here
+
